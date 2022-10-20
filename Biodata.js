6d3accd6e767c5206a676fadb275d2c281847b98
Deepavishali <115686314+Deepavishali@users.noreply.github.com>
2022-10-20 22:39:33 +0530

    Add files via upload

---
+++ Biodata.js
@@ -0,0 +1,51 @@
+const personalinfo = [
+    {
+        "Name" : "Deepa vishali G",
+        "Fathername" : "Ganesan J",
+        "DOB" : "01-04-2002",
+        "Native" : "Madurai",
+        "Nationality" : "Indian",
+        "Email ID" : "deepavisahlig@gmail.com",
+        "Mobile number" : "8681xxxxx6",
+        "Address" : "52,West masi street"
+
+    },
+    {
+       "Qualification" : "Bsc mathematics with computer Applications",
+       "Work Experience" : "LDC Eco Bridge-(LEB)",
+    },
+]
+console.log("Skills")
+const skills = [{"1" : "Analytical Skills"},
+                 {"2": "Communication and Interpersonal skills"},
+                 {"3" : "javascript"},
+                 {"4" : "java"}]             
+const certificates = ["DCA","PGDMA"]
+const Languages = ["Telugu","Tamil","English"]
+const Interest = ["Pencil sketching","Cooking","Gardening"]
+
+console.log("Personal Details :" )
+
+console.log(personalinfo)
+
+console.log("Skills : " )
+
+console.log(skills)
+
+console.log("certificates : " )
+
+console.log(certificates[0])
+console.log(certificates[1])
+
+console.log("Languages : " )
+
+console.log(Languages[0])
+console.log(Languages[1])
+console.log(Languages[2])
+
+console.log("Interest : " )
+
+console.log(Interest[0])
+console.log(Interest[1])
+console.log(Interest[2])
+
